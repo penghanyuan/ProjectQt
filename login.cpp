79cf06314266b90010743f8fb0c4c537e33e52af
ywlqjl <yanwenliqjl@gmail.com>
2018-03-10 12:34:07 +0100

    optimize the login and other interface

--- login.cpp
+++ login.cpp
@@ -1,11 +1,19 @@
 #include "login.h"
 #include "ui_login.h"
+#include <QSettings>
+#include <QDebug>
+
 
 Login::Login(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Login)
 {
     ui->setupUi(this);
+    ui->username->setFocus();
+    loadcfg();
+    ui->username->setText(login_username);
+    ui->password->setText(login_password);
+
 }
 
 Login::~Login()
@@ -15,15 +23,52 @@ Login::~Login()
 
 void Login::on_connect_clicked()
 {
-    QString username = ui->username->text();
-    QString password = ui->password->text();
     LoginController logController;
-    if(logController.verifyLogin(username,password)){
-        accept();
-    }else{
-        QMessageBox::warning(this, tr("Waring"),
-                                     tr("user name or password error!"),
-                                     QMessageBox::Ok);
-        ui->password->clear();
+
+    if(login_username.isEmpty()&&login_password.isEmpty())
+    {
+        login_username = ui->username->text();
+        login_password = ui->password->text();
     }
+    if(logController.verifyLogin(login_username,login_password)){
+        savecfg();
+        accept();
+    }
+    else{
+            QMessageBox::warning(this, tr("Waring"),
+                                         tr("user name or password error!"),
+                                         QMessageBox::Ok);
+            ui->password->clear();
+            ui->password->setFocus();
+            login_username.clear();
+            login_password.clear();
+        }
+
+
+
+}
+
+void Login::savecfg()
+{
+    QString path = "login.ini";
+    QSettings *config = new QSettings(path, QSettings::IniFormat);
+    login_username = ui->username->text();
+    login_password = ui->password->text();
+    config->setValue("username", login_username);
+    config->setValue("password", login_password);
+    delete config;
+
+}
+
+
+void Login::loadcfg()
+{
+    QString path = "login.ini";
+    QSettings *config = new QSettings(path, QSettings::IniFormat);
+    login_username = config->value("username").toString();
+    login_password = config->value("password").toString();
+
+    delete config;
+
+
 }
